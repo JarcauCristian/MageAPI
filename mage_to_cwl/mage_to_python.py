4a30e85feb3269240bbd80b9d6d9f7bc7b8a12ad
Jarcau Stefan-Cristian <stefan.jarcau@student.unitbv.ro>
2024-07-13 16:48:56 +0300

    Done the first step

--- mage_to_cwl/mage_to_python.py
+++ mage_to_cwl/mage_to_python.py
@@ -7,7 +7,7 @@ import os
 
 
 class MageToPython:
-    def __init__(self, code_string: str, block_name: str, previous_block_name: str = None, input_type: str = "pandas") -> None:
+    def __init__(self, code_string: str, block_name: str, previous_block_name: str = None) -> None:
         """
         Initializer function for MageToPython class.
         :param code_string: The string that contains the Mage AI formatted Python code .
@@ -16,8 +16,6 @@ class MageToPython:
         self.env_vars = None
         self.block_name = block_name
         self.previous_block_name = previous_block_name
-        self.input_type = input_type
-        self.output_type = None
 
     def _format_code_autopep8(self) -> None:
         with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as tmp_file:
@@ -59,7 +57,7 @@ class MageToPython:
 
     def _remove_mage_imports(self) -> None:
         self.code_string, self.env_vars = replace_code_patterns(self.code_string)
-        self.code_string, env_vars, self.output_type = remove_imports_with_word(self.code_string, "mage_ai", self.input_type, self.block_name, self.previous_block_name)
+        self.code_string, env_vars = remove_imports_with_word(self.code_string, "mage_ai", self.block_name, self.previous_block_name)
         self.env_vars = self.env_vars + env_vars
 
     def mage_to_python(self) -> None:
@@ -68,70 +66,7 @@ class MageToPython:
         self._format_code_black()
 
     def __str__(self):
-        return f"MageToPython(code_string={self.code_string}, env_vars={self.env_vars}, output_type={self.output_type})"
+        return f"MageToPython(code_string={self.code_string}, env_vars={self.env_vars})"
 
     def __repr__(self):
-        return f"MageToPython(code_string={self.code_string}, env_vars={self.env_vars}, output_type={self.output_type})"
-
-# string = """
-# # Variables {"username":{"type":"str","description":"The username for the user to login inside the database.","regex":"^.*$"},"password":{"type":"secret","description":"The password for the user to login inside the database."},"host":{"type":"str","description":"The host address where the database resides.","regex":"^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$"},"port":{"type":"int","description":"The port on which the database runs.","range":[0,65535]},"database":{"type":"str","description":"The name of the database.","regex":"^.*$"},"collection":{"type":"str","description":"The name of the collection to load data from.","regex":"^.*$"}}
-#
-# import pandas as pd
-# from pymongo import MongoClient
-# from mage_ai.data_preparation.shared.secrets import get_secret_value
-#
-# if 'data_loader' not in globals():
-#     from mage_ai.data_preparation.decorators import data_loader
-# if 'test' not in globals():
-#     from mage_ai.data_preparation.decorators import test
-#
-# def some_other_function():
-#     print("Here!")
-#
-# @data_loader
-# def load_data_mongodb(data, *args, **kwargs):
-#     \"""
-#     Template code for loading data from a MongoDB database.
-#
-#     Args:
-#     - kwargs should include 'username', 'password', 'host', 'port', 'database', and 'table'.
-#
-#     Returns:
-#         pandas.DataFrame - Data loaded from the specified MongoDB collection.
-#     \"""
-#
-#     username = kwargs.get('username')
-#     host = kwargs.get('host')
-#     port = kwargs.get('port')
-#     database = kwargs.get('database')
-#     collection = kwargs.get('collection')
-#
-#     secret_name = "password-" + kwargs.get("PIPELINE_NAME")
-#
-#     password = get_secret_value(secret_name)
-#
-#     if None in [username, password, host, port, database, collection]:
-#         raise ValueError("All connection parameters (username, password, host, port, database, collection) must be provided.")
-#
-#     connection_string = f"mongodb://{username}:{password}@{host}:{port}/{database}"
-#
-#     client = MongoClient(connection_string)
-#     db = client[database]
-#     collection = db[collection]
-#
-#     df = pd.DataFrame(list(collection.find()))
-#
-#     return df
-#
-# @test
-# def test_output(output, *args) -> None:
-#     \"""
-#     Template code for testing the output of the block.
-#     \"""
-#     assert output is not None, 'The output is undefined'
-#     assert isinstance(output, pd.DataFrame), 'Output is not a DataFrame'
-#
-# """
-# mtp = MageToPython(string, "random", "test", "pandas")
-# mtp.mage_to_python()
-# print(mtp.code_string, mtp.env_vars, mtp.output_type)
+        return f"MageToPython(code_string={self.code_string}, env_vars={self.env_vars})"
