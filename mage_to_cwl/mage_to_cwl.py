abed5c775a6bf6f6cf26bdcd2df59e30c2d778a7
Jarcau Stefan-Cristian <stefan.jarcau@student.unitbv.ro>
2024-07-13 07:55:38 +0300

    Closer and closer

--- mage_to_cwl/mage_to_cwl.py
+++ mage_to_cwl/mage_to_cwl.py
@@ -1,7 +1,5 @@
-from typing import List, Dict, Any
-
 import yaml
-
+from typing import List, Dict, Any
 from mage_to_cwl.mage_to_python import MageToPython
 
 
@@ -25,6 +23,17 @@ class MageToCWL:
                 entry.mage_to_python()
                 self.results.append(entry)
 
+    class QuotedString(str):
+        pass
+
+    @staticmethod
+    def _quoted_str_representer(dumper, data):
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+
+    @staticmethod
+    def _list_representer(dumper, data):
+        return dumper.represent_sequence('tag:yaml.org,2002:seq', data, flow_style=True)
+
     @staticmethod
     def _first_step_template() -> str:
         string = """
@@ -33,7 +42,7 @@ class MageToCWL:
         baseCommand: [bash, -c]
 
         inputs:
-          block_name:
+          block_name_script:
             type: string
             inputBinding:
               position: 1
@@ -47,10 +56,10 @@ class MageToCWL:
               position: 3
 
         outputs:
-          block_name:
+          block_name_result:
             type: File
             outputBinding:
-              glob: block_name
+              glob: block_name_result
               
         arguments:
           - valueFrom: |
@@ -72,12 +81,12 @@ class MageToCWL:
         baseCommand: [bash, -c]
 
         inputs:
-          block_name:
+          block_name_script:
             type: string
             inputBinding:
               position: 1
-          prev_block_name:
-            type: string
+          prev_block_name_result:
+            type: File
             inputBinding:
               position: 2
           scripts_directory:
@@ -86,16 +95,16 @@ class MageToCWL:
               position: 3
 
         outputs:
-          block_name:
+          block_name_result:
             type: File
             outputBinding:
-              glob: block_name
+              glob: block_name_result
               
         arguments:
           - valueFrom: |
               set -e && \
               export PYTHONPATH=$3:$PYTHONPATH && \
-              python3 $(inputs.scripts_directory.path)/$(inputs.block_name)
+              python3 $(inputs.scripts_directory.path)/$(inputs.block_name) $2
         
           - position: 0
             prefix: --
@@ -133,14 +142,14 @@ class MageToCWL:
           requirements_file:
             type: File
             outputBinding:
-              glob: scripts/requirements.txt
+              glob: scripts/requirements/requirements.txt
         
         arguments:
           - valueFrom: |
               set -e
               export PATH=$HOME/.local/bin:$PATH && \
               pip install --user pipreqs && \
-              pipreqs --force $1/requirements && \
+              pipreqs --force $1 --savepath $1/requirements && \
               pip install --target $1/requirements -r $1/requirements/requirements.txt
         
           - position: 0
@@ -177,18 +186,20 @@ class MageToCWL:
 
     def process(self) -> None:
         self._transform_mage_to_python()
+        yaml.representer.SafeRepresenter.add_representer(self.QuotedString, self._quoted_str_representer)
+        yaml.representer.SafeRepresenter.add_representer(list, self._list_representer)
         inputs = self._inputs()
         workflow = self._workflow()
         self.files[f"{self.pipeline_name}/steps/install_requirements.cwl"] = self._install_script()
         for i, result in enumerate(self.results):
             self.files[f"{self.pipeline_name}/scripts/{result.block_name}.py"] = result.code_string
-            inputs[result.block_name] = f"{result.block_name}.py"
-            workflow["inputs"][result.block_name] = {
+            inputs[f"{result.block_name}_script"] = f"{result.block_name}.py"
+            workflow["inputs"][f"{result.block_name}_script"] = {
                 "type": "string"
             }
-            workflow["outputs"][result.block_name] = {
+            workflow["outputs"][f"{result.block_name}_result"] = {
                 "type": "File",
-                "outputSource": f"{result.block_name}/{result.block_name}"
+                "outputSource": f"{result.block_name}/{result.block_name}_result"
             }
             if i == 0:
                 template = self._first_step_template().replace("block_name", result.block_name)
@@ -196,11 +207,11 @@ class MageToCWL:
                 workflow["steps"][result.block_name] = {
                     "run": f"./steps/{result.block_name}.cwl",
                     "in": {
-                        result.block_name: result.block_name,
+                        f"{result.block_name}_script": f"{result.block_name}_script",
                         "scripts_directory": "scripts_folder",
                         "dependency": "install_requirements/requirements_file"
                     },
-                    "out": f"[{result.block_name}]"
+                    "out": [f"{result.block_name}_result"]
                 }
             else:
                 template = self._step_template()
@@ -210,20 +221,20 @@ class MageToCWL:
                 workflow["steps"][result.block_name] = {
                     "run": f"./steps/{result.block_name}.cwl",
                     "in": {
-                        result.block_name: result.block_name,
-                        result.previous_block_name: result.previous_block_name,
+                        f"{result.block_name}_script": f"{result.block_name}_script",
+                        f"{result.previous_block_name}_result": f"{result.previous_block_name}/{result.previous_block_name}_result",
                         "scripts_directory": "scripts_folder"
                     },
-                    "out": f"[{result.block_name}]"
+                    "out": [f"{result.block_name}_result"]
                 }
 
-            self.files[f"{self.pipeline_name}/steps/{result.block_name}.cwl"] = yaml.safe_dump(yaml.safe_load(template))
+            self.files[f"{self.pipeline_name}/steps/{result.block_name}.cwl"] = yaml.safe_dump(yaml.safe_load(template), default_flow_style=False, sort_keys=False)
 
             for env in result.env_vars:
                 if "OUTPUT_FILE" in env:
-                    workflow["requirements"]["EnvVarRequirement"]["envDef"][env] = f'\"{env.split("_OUTPUT_FILE")[0].lower()}\"'
+                    workflow["requirements"]["EnvVarRequirement"]["envDef"][env] = self.QuotedString(f"{env.split('_OUTPUT_FILE')[0].lower()}_result")
                 else:
-                    workflow["requirements"]["EnvVarRequirement"]["envDef"][env] = "\"PLACEHOLDER"
+                    workflow["requirements"]["EnvVarRequirement"]["envDef"][env] = "PLACEHOLDER"
 
         self.files[f"{self.pipeline_name}/inputs.yml"] = yaml.safe_dump(inputs)
-        self.files[f"{self.pipeline_name}/workflow.cwl"] = yaml.safe_dump(workflow)
+        self.files[f"{self.pipeline_name}/workflow.cwl"] = yaml.safe_dump(workflow, default_flow_style=False, sort_keys=False)
